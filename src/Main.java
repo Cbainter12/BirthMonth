bf41a977f09c8f4d651f0fda67f7beaae9fcd2e5
Carter <carterleebainter@gmail.com>
2023-09-22 16:35:22 -0400

    Final

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,14 @@ public class Main {
     {
         Scanner in = new Scanner(System.in);
         int birthMonth = 0;
-        System.out.println("Enter your birth month [1-12]: ");
+        System.out.println("Enter your Birth month [1-12]: ");
         if (in.hasNextInt()) {
             birthMonth = in.nextInt();
             in.nextLine();
             if (birthMonth >= 1 && birthMonth <= 12) {
-                System.out.println("You said your birth month is: " + birthMonth);
+                System.out.println("You said your Birth month is: " + birthMonth);
             } else {
-                System.out.println("You said your birth month is: " + birthMonth);
+                System.out.println("You said your Birth month is: " + birthMonth);
                 System.out.println("But, it has to be [1-12] and it isn't.");
             }
         }
